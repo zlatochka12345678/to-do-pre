ffc02f3c8148547651e0cf32d435b32055a2442a
zlatochka12345678 <zlatasuhareva2009@gmail.com>
2026-04-21 13:17:36 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let items = [
+	"Сделать проектную работу",
+	"Полить цветы",
+	"Пройти туториал по Реакту",
+	"Сделать фронт для своего проекта",
+	"Прогуляться по улице в солнечный день",
+	"Помыть посуду",
+];
+
+const listElement = document.querySelector(".to-do__list");
+const formElement = document.querySelector(".to-do__form");
+const inputElement = document.querySelector(".to-do__input");
+
+function loadTasks() {
+
+}
+
+function createItem(item) {
+	const template = document.getElementById("to-do__item-template");
+	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+  const textElement = clone.querySelector(".to-do__item-text");
+  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+
+}
+
+function getTasksFromDOM() {
+
+}
+
+function saveTasks(tasks) {
+
+}
+
